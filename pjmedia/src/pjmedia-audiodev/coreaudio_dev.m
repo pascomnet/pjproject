552a1c7bae37a599e7d54b48b9bf82da51ccd645
Jan Lorenz <jan.lorenz@pascom.net>
2023-05-05 12:11:56 +0200

    CL-3519 null checks before calling CFRelease

--- pjmedia/src/pjmedia-audiodev/coreaudio_dev.m
+++ pjmedia/src/pjmedia-audiodev/coreaudio_dev.m
@@ -559,7 +559,9 @@ static pj_status_t ca_factory_refresh(pjmedia_aud_dev_factory *f)
     } else {
         PJ_LOG(1, (THIS_FILE, " failed to copy id to buffer"));
     }
-    CFRelease(uid);
+    if(uid != NULL) {
+        CFRelease(uid);
+    }
     
     /* Get device name */
     addr.mSelector = kAudioDevicePropertyDeviceName;
@@ -2006,7 +2008,9 @@ static pj_status_t ca_stream_get_cap(pjmedia_aud_stream *s,
 	    *(pjmedia_aud_dev_route*)pval = PJMEDIA_AUD_DEV_ROUTE_DEFAULT;
 	}
 
-	CFRelease(route);
+    if(route != NULL) {
+	    CFRelease(route);
+    }
 
 	return PJ_SUCCESS;
 #endif
